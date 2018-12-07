7cfeba18974f17cf20dfa1c574c7d276d4854151
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2018-12-07 18:28:45 +0530

    remove if in head function
    use object

--- src/headLibrary.js
+++ src/headLibrary.js
@@ -1,52 +1,45 @@
-const getLines = function(content, count) {
-  let result = content.split("\n").slice(0, count);
-  return result.join("\n");
+const getLines = function(content,count){
+  let result = content.split('\n').slice(0,count);
+  return result.join('\n');
 };
 
-const getCharacters = function(content, character) {
-  let result = content.split("").slice(0, character);
-  return result.join("");
+const getCharacters = function(content,character){
+  let result = content.split('').slice(0,character);
+  return result.join('');
 };
 
-const errorHandling = function({ option, count, inpputFiles }) {
-  if (option != "n" && option != "c") {
-    return (
-      "head: illegal option -- " +
-      option +
-      "\n" +
-      "usage: head [-n lines | -c bytes] [file ...]"
-    );
+const errorHandling = function({option,count,inpputFiles}){
+  if (option != 'n' && option !='c') {
+    return 'head: illegal option -- ' + option + '\n' + 'usage: head [-n lines | -c bytes] [file ...]';
   }
   if (isNaN(count - 0) || count < 1) {
-    if (option == "n") {
-      return "head: illegal line count -- " + count;
+    if(option == 'n'){
+      return 'head: illegal line count -- ' + count;
     }
-    return "head: illegal byte count -- " + count;
+    return 'head: illegal byte count -- ' + count;
   }
-};
+}  
 
-const head = function(readFileSync, validater, { option, count, inputFiles }) {
-  let error = errorHandling({ option, count, inputFiles });
-  if (error) {
-    return error;
-  }
-  return inputFiles
-    .map(function(file) {
-      if (!validater(file)) {
-        return "head: " + file + ": No such file or directory";
-      }
-      let content = readFileSync(file, "utf8");
-      let result = getLines(content, count);
-      let fileName = "==> " + file + " <==" + "\n";
-      if (option == "c") {
-        result = getCharacters(content, count);
-      }
-      if (inputFiles.length > 1) {
-        return fileName + result + "\n";
-      }
-      return result;
-    })
-    .join("\n");
-};
+const head = function(readFileSync,validater,{option,count,inputFiles}){
+  let typeCall = {'n':getLines, 'c':getCharacters };
+  let error = errorHandling({option,count,inputFiles});
+    if(error) {return error};
+  return inputFiles.map(function(file){
+    if (!validater(file)) {
+      return 'head: '+file+': No such file or directory';
+    }
 
-module.exports = { getLines, getCharacters, errorHandling, head };
+    let content = readFileSync(file,'utf8');
+    let result = typeCall[option](content,count);
+    let fileName = "==> " + file + " <==" + '\n';
+    if(inputFiles.length > 1){
+      return fileName + result + '\n';
+    }
+    return result;
+  }).join('\n');
+}
+
+
+module.exports = {getLines,getCharacters,errorHandling,head};
+
+
