97337d6bff4e4e34ce2ea1f87c67c297c8a9ae2b
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2018-12-07 15:06:29 +0530

    Refactor the head function extrect the error haldling function.
    test added for error handling function.

--- src/utilLib.js
+++ src/utilLib.js
@@ -1,20 +1,32 @@
-const inputSeparator = function(userInputs){
-  let separatedInputs = {option:'n',count:10,inputFiles:userInputs.slice(0)}
-  if( userInputs[0].length >= 3 && userInputs[0][0] =='-' && isNaN(userInputs[0][1]) ) {
+const inputSeparator = function(userInputs) {
+  let separatedInputs = {
+    option: "n",
+    count: 10,
+    inputFiles: userInputs.slice(0)
+  };
+  if (
+    userInputs[0].length >= 3 &&
+    userInputs[0][0] == "-" &&
+    isNaN(userInputs[0][1])
+  ) {
     separatedInputs.option = userInputs[0][1];
     separatedInputs.count = userInputs[0].slice(2);
     separatedInputs.inputFiles = userInputs.slice(1);
   }
-  if( userInputs[0].length >= 2 && !isNaN(userInputs[0].slice(0,2)) ) {
+  if (userInputs[0].length >= 2 && !isNaN(userInputs[0].slice(0, 2))) {
     separatedInputs.count = userInputs[0].slice(1);
     separatedInputs.inputFiles = userInputs.slice(1);
   }
-  if( userInputs[0].length == 2 && isNaN(userInputs[0][1]) && userInputs[0][0]=='-') {
+  if (
+    userInputs[0].length == 2 &&
+    isNaN(userInputs[0][1]) &&
+    userInputs[0][0] == "-"
+  ) {
     separatedInputs.option = userInputs[0][1];
     separatedInputs.count = userInputs[1];
     separatedInputs.inputFiles = userInputs.slice(2);
   }
   return separatedInputs;
-}
+};
 
 exports.inputSeparator = inputSeparator;
--- test/utilLibTest.js
+++ test/utilLibTest.js
@@ -1,29 +1,36 @@
-const assert = require('assert');
-const {inputSeparator} = require('../src/utilLib.js')
+const assert = require("assert");
+const { inputSeparator } = require("../src/utilLib.js");
 
-describe('inputSeparator', function() {
-  it('should return parameter object when line/byte and lines is provided combine', function() {
-    let actualInput = inputSeparator(['-n5','file1']); 
-    let expectedOutput = {option:'n',count:'5',inputFiles:['file1']};
-    assert.deepEqual(actualInput,expectedOutput);
+describe("inputSeparator", function() {
+  it("should return parameter object when line/byte and lines is provided combine", function() {
+    let actualInput = inputSeparator(["-n5", "file1"]);
+    let expectedOutput = { option: "n", count: "5", inputFiles: ["file1"] };
+    assert.deepEqual(actualInput, expectedOutput);
   });
 
-  it('should return parameter object when only lines is provide', function() {
-    let actualInput = inputSeparator(['-5','file1']); 
-    let expectedOutput = {option:'n',count:'5',inputFiles:['file1']};
-    assert.deepEqual(actualInput,expectedOutput);
+  it("should return parameter object when only lines is provide", function() {
+    let actualInput = inputSeparator(["-5", "file1"]);
+    let expectedOutput = { option: "n", count: "5", inputFiles: ["file1"] };
+    assert.deepEqual(actualInput, expectedOutput);
   });
 
-  it('should return parameter object when line/byte and lines is provided separately', function() {
-    let actualInput = inputSeparator(['-c','3','file1','file2']); 
-    let expectedOutput = {option:'c',count:'3',inputFiles:['file1','file2']};
-    assert.deepEqual(actualInput,expectedOutput);
+  it("should return parameter object when line/byte and lines is provided separately", function() {
+    let actualInput = inputSeparator(["-c", "3", "file1", "file2"]);
+    let expectedOutput = {
+      option: "c",
+      count: "3",
+      inputFiles: ["file1", "file2"]
+    };
+    assert.deepEqual(actualInput, expectedOutput);
   });
 
-  it('should return parameter object when only inputs files are provided', function() {
-    let actualInput = inputSeparator(['file1','file2']); 
-    let expectedOutput = {option:'n',count:'10',inputFiles:['file1','file2']};
-    assert.deepEqual(actualInput,expectedOutput);
+  it("should return parameter object when only inputs files are provided", function() {
+    let actualInput = inputSeparator(["file1", "file2"]);
+    let expectedOutput = {
+      option: "n",
+      count: "10",
+      inputFiles: ["file1", "file2"]
+    };
+    assert.deepEqual(actualInput, expectedOutput);
   });
 });
-
